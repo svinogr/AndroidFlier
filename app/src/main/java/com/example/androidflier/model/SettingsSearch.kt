85d538d724cd769d63fb03be58d592437e63d4a7
Сергей <!@>
2021-12-20 10:22:11 +0300

    ref ProfileFragment.kt

--- app/src/main/java/com/example/androidflier/model/SettingsSearch.kt
+++ app/src/main/java/com/example/androidflier/model/SettingsSearch.kt
@@ -4,6 +4,6 @@ data class SettingsSearch(
     var on: Boolean = true ,
     var timePeriod: Int = 0,
     var radius: Int = 4,
-    var listTag: MutableList<String> = mutableListOf()
+    var listTag: List<String> = listOf()
 ) {
 }
